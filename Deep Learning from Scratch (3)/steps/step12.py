c6f5851d0b810f6fe78c7b3a8ec7991ba870dfcb
markFT39 <43339545+markFT39@users.noreply.github.com>
2025-09-29 21:28:45 +0900

    [3-Ch02] Step11 ~ Step17 가변 길이 인수, 메모리 관리
    - 함수의 가변 길이 인수 구현
    - 역전파 시 복잡한 계산 그래프의 미분 스케줄링
    - 순환 참조 문제와 메모리 관리 개선

--- Deep Learning from Scratch (3)/steps/step12.py
+++ Deep Learning from Scratch (3)/steps/step12.py
@@ -1,5 +1,120 @@
 import numpy as np
+import unittest
 
+class Variable:
+    def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError('{}은(는) 지원하지 않습니다.'.format(type(data)))
 
+        self.data = data
+        self.grad = None
+        self.creator = None
 
-# if __name__ == '__main__':
+    def set_creator(self, func):
+        self.creator = func
+
+    def backward(self):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
+        funcs = [self.creator]
+        while funcs:
+            f = funcs.pop()
+            x, y = f.input, f.output
+            x.grad = f.backward(y.grad)
+
+            if x.creator is not None:
+                funcs.append(x.creator)
+
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+
+class Function:
+    def __call__(self, *inputs):        # *는 가변 길이 인수
+        xs = [x.data for x in inputs]
+        ys = self.forward(*xs)          # *를 붙여서 언팩함, 리스트의 원소를 낱개로 풀어서 전달
+        if not isinstance(ys, tuple):   # 튜플이 아닌 경우 튜플로 변환
+            ys = (ys,)
+        outputs = [Variable(as_array(y)) for y in ys]
+
+        for output in outputs:
+            output.set_creator(self)
+        self.inputs = inputs
+        self.outputs = outputs
+        return outputs if len(outputs) > 1 else outputs[0]
+
+    def forward(self, xs):
+        raise NotImplementedError()
+
+    def backward(self, gys):
+        raise NotImplementedError()
+    
+class Add(Function):
+    def forward(self, x0, x1):
+        y = x0 + x1
+        return y
+    
+class Square(Function):
+    def forward(self, x):
+        return x ** 2
+    
+    def backward(self, gy):
+        x = self.input.data
+        gx = 2 * x * gy
+        return gx
+
+class Exp(Function):
+    def forward(self, x):
+        return np.exp(x)
+    
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
+
+def square(x):
+    return Square()(x)  
+
+def exp(x):
+    return Exp()(x)
+
+def add(x0, x1):
+    return Add()(x0, x1)
+
+def numerical_diff(f, x, eps=1e-4):
+    x0 = Variable(x.data - eps)
+    x1 = Variable(x.data + eps)
+    y0 = f(x0)
+    y1 = f(x1)
+    return (y1.data - y0.data) / (2 * eps)
+
+class SquareTest(unittest.TestCase):
+    def test_forward(self):
+        x = Variable(np.array(2.0))
+        y = square(x)
+        expected = np.array(4.0)
+        self.assertEqual(y.data, expected)
+
+    def test_backward(self):
+        x = Variable(np.array(3.0))
+        y = square(x)
+        y.backward()
+        expected = np.array(6.0)
+        self.assertEqual(x.grad, expected)
+
+    def test_gradient_check(self):
+        x = Variable(np.random.rand(1))     # 무작위 값 생성
+        y = square(x)
+        y.backward()
+        num_grad = numerical_diff(square, x)
+        flg = np.allclose(x.grad, num_grad)     # 수치 미분값과 역전파 값이 서로 비슷한지 판별
+        self.assertTrue(flg)
+
+if __name__ == '__main__':
+    x0 = Variable(np.array(2))
+    x1 = Variable(np.array(3))
+    y = add(x0, x1)
+    print(y.data)
