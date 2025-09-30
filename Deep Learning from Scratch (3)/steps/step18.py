30157447fe366731dab9893cca7d427751117386
markFT39 <43339545+markFT39@users.noreply.github.com>
2025-09-30 19:32:40 +0900

    [3-Ch02] Step18 ~ Step21 메모리 절약, 연산자 오버로드
    - 순전파 수행시 중간 미분 변수값 유지 설정 추가
    - 연산자 오버로드를 통한 사용성 개선(+, * 등)
    - ndarray 인스턴스 변수 설정

--- Deep Learning from Scratch (3)/steps/step18.py
+++ Deep Learning from Scratch (3)/steps/step18.py
@@ -1,5 +1,194 @@
 import numpy as np
+import unittest
+import weakref
+import contextlib
 
+class Variable:
+    def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError('{}은(는) 지원하지 않습니다.'.format(type(data)))
 
+        self.data = data
+        self.grad = None
+        self.creator = None
+        self.generation = 0     
 
-# if __name__ == '__main__':
+    def set_creator(self, func):
+        self.creator = func
+        self.generation = func.generation + 1  
+
+    def backward(self, retain_grad=False):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
+        funcs = []
+        seen_set = set()
+
+        def add_func(f):
+            if f not in seen_set:
+                funcs.append(f)
+                seen_set.add(f)
+                funcs.sort(key=lambda x: x.generation)
+
+        add_func(self.creator)
+
+        while funcs:
+            f = funcs.pop()
+            gys = [output().grad for output in f.outputs]
+            gxs = f.backward(*gys)
+            if not isinstance(gxs, tuple):
+                gxs = (gxs,)
+
+            for x, gx in zip(f.inputs, gxs):
+                if x.grad is None:
+                    x.grad = gx
+
+                else:
+                    x.grad = x.grad + gx
+
+                if x.creator is not None:
+                    add_func(x.creator)
+            if not retain_grad:
+                for y in f.outputs:
+                    y().grad = None     # y는 약한 참조로 y()로 사용, retain이 False일 경우 메모리 절약
+
+    def cleargrad(self):
+        self.grad = None
+
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+
+class Config:
+    # 역전파 기능 활성 여부 설정 변수
+    enable_backdrop = True
+
+class Function:
+    def __call__(self, *inputs):        
+        xs = [x.data for x in inputs]
+        ys = self.forward(*xs)          
+        if not isinstance(ys, tuple):  
+            ys = (ys,)
+        outputs = [Variable(as_array(y)) for y in ys]
+
+        if Config.enable_backdrop:
+            self.generation = max([x.generation for x in inputs])
+            for output in outputs:
+                output.set_creator(self)
+            self.inputs = inputs
+            self.outputs = [weakref.ref(output) for output in outputs]  # 약한 참조로 변경, 순환 참조시 GC가 제거할 수 있음
+        return outputs if len(outputs) > 1 else outputs[0]
+
+    def forward(self, xs):
+        raise NotImplementedError()
+
+    def backward(self, gys):
+        raise NotImplementedError()
+    
+class Add(Function):
+    def forward(self, x0, x1):
+        y = x0 + x1
+        return y
+    
+    def backward(self, gy):
+        return gy, gy
+    
+class Square(Function):
+    def forward(self, x):
+        return x ** 2
+    
+    def backward(self, gy):
+        x = self.inputs[0].data
+        gx = 2 * x * gy
+        return gx
+
+class Exp(Function):
+    def forward(self, x):
+        return np.exp(x)
+    
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
+
+def square(x):
+    return Square()(x)  
+
+def exp(x):
+    return Exp()(x)
+
+def add(x0, x1):
+    return Add()(x0, x1)
+
+def numerical_diff(f, x, eps=1e-4):
+    x0 = Variable(x.data - eps)
+    x1 = Variable(x.data + eps)
+    y0 = f(x0)
+    y1 = f(x1)
+    return (y1.data - y0.data) / (2 * eps)
+
+class SquareTest(unittest.TestCase):
+    def test_forward(self):
+        x = Variable(np.array(2.0))
+        y = square(x)
+        expected = np.array(4.0)
+        self.assertEqual(y.data, expected)
+
+    def test_backward(self):
+        x = Variable(np.array(3.0))
+        y = square(x)
+        y.backward()
+        expected = np.array(6.0)
+        self.assertEqual(x.grad, expected)
+
+    def test_gradient_check(self):
+        x = Variable(np.random.rand(1))     
+        y = square(x)
+        y.backward()
+        num_grad = numerical_diff(square, x)
+        flg = np.allclose(x.grad, num_grad)     
+        self.assertTrue(flg)
+
+@contextlib.contextmanager
+def using_config(name, value):
+    old_value = getattr(Config, name)
+    setattr(Config, name, value)
+    try:
+        yield
+    finally:
+        setattr(Config, name, old_value)
+
+def no_grad():
+    return using_config('enable_backdrop', False)
+
+if __name__ == '__main__':
+    x0 = Variable(np.array(1.0))
+    x1 = Variable(np.array(1.0))
+    t = add(x0, x1)
+    y = add(x0, t)
+    y.backward()
+
+    print(y.grad, t.grad)
+    print(x0.grad, x1.grad)
+
+    # 중간 계산 과정에서 메모리 유지
+    Config.enable_backdrop = True
+    x = Variable(np.ones((100, 100, 100)))
+    y = square(square(square(x)))
+    y.backward()
+
+    # 중간 계산의 경우 계산 완료시 메모리에서 삭제
+    Config.enable_backdrop = False
+    x = Variable(np.ones((100, 100, 100)))
+    y = square(square(square(x)))
+
+    # 역전파를 False로 임시 설정하고(기울기가 필요 없는 경우) 하위 코드 실행 후 finally에서 본래 Config 값을 복원함
+    with using_config('enable_backdrop', False):
+        x = Variable(np.array(2.0))
+        y = square(x)
+
+    with no_grad():
+        x = Variable(np.array(2.0))
+        y = square(x)
