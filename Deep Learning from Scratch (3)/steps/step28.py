1f84326474b1e69df346854e4e70ede24751fb55
markFT39 <43339545+markFT39@users.noreply.github.com>
2025-10-02 19:54:58 +0900

    [3-Ch03] Step25 ~ Step28 그래프 시각화, 함수 최적화
    - graphviz를 통한 계산 그래프 시각화 구현
    - 테일러급수(매클로린 전개) 구현 및 검증
    - 로젠브록 함수를 통한 함수 최적화 실습

--- Deep Learning from Scratch (3)/steps/step28.py
+++ Deep Learning from Scratch (3)/steps/step28.py
@@ -1,5 +1,35 @@
+if '__file__' in globals():
+    import os, sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
 import numpy as np
+from dezero import Variable, setup_variable
 
+def rosenbrock(x0, x1):
+    y = 100 * (x1 - x0 ** 2) ** 2 + (1 - x0) ** 2
+    return y
 
+if __name__ == '__main__':
+    x0 = Variable(np.array(0.0))
+    x1 = Variable(np.array(2.0))
 
-# if __name__ == '__main__':
+    y = rosenbrock(x0, x1)
+    y.backward()
+
+    print(x0.grad, x1.grad)
+
+    lr = 0.001
+    iters = 50000
+
+    # 경사하강법으로 계산해도 최솟값인 (1.0, 1.0)에 도달하기 어렵다. (계산량이 많음)
+    for i in range(iters):
+        print(x0, x1)
+
+        y = rosenbrock(x0, x1)
+
+        x0.cleargrad()
+        x1.cleargrad()
+        y.backward()
+
+        x0.data -= lr * x0.grad
+        x1.data -= lr * x1.grad
