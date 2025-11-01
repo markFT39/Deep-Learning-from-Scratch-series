607c690c9d8860924bfcf8642514dc6531d0904a
markFT39 <43339545+markFT39@users.noreply.github.com>
2025-11-01 15:22:34 +0900

    [4-Ch08] DQN(Deep Q Network)
    - DQN 개념 학습 및 구현
    - OpenAI gym의 cart pole 문제
    - 경험 재생(experience replay) 개념 학습 및 구현

---
+++ Deep Learning from Scratch (4)/Ch08/gym_play.py
@@ -0,0 +1,16 @@
+import numpy as np
+if '__file__' in globals():
+    import sys
+    sys.path.append("C:/Users/qps10/anaconda3/Lib/site-packages")
+import gym
+
+env = gym.make('CartPole-v1', render_mode='human')
+state = env.reset()[0]
+done = False
+
+while not done:  # 에피소드가 끝날 때까지 반복
+    env.render()  # 진행 과정 시각화
+    action = np.random.choice([0, 1])  # 행동 선택(무작위)
+    next_state, reward, terminated, truncated, info = env.step(action)
+    done = terminated | truncated      # 둘 중 하나만 True면 에피소드 종료
+env.close()
